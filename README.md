7cbf9f47cc2f5cdec441ec6d75c15787021e7a17
tuanx31 <114796871+tuanx31@users.noreply.github.com>
2023-03-08 21:30:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# lda3001.github.io
---
+++ js/music-list.js
@@ -0,0 +1,104 @@
+let allMusic = [{
+        name: "Trời Giấu Trời Mang Đi",
+        artist: "Lê Đức Anh",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Giấc Mơ Rất Thơ",
+        artist: "Lê Đức Anh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Tình Thật Tình Mơ",
+        artist: "Lê Đức Anh",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Đắp Mộ Magaming",
+        artist: "Lê Đức Anh",
+        img: "2",
+        src: "2"
+    },
+    {
+        name: "Thương Cho Tấm Thân",
+        artist: "Lê Đức Anh",
+        img: "3",
+        src: "3"
+    },
+    {
+        name: "Thà Yêu Lấy Một Người Bình Thường",
+        artist: "Lê Đức Anh",
+        img: "4",
+        src: "4"
+    },
+    {
+        name: "Ngàn Câu Hứa",
+        artist: "Lê Đức Anh",
+        img: "5",
+        src: "5"
+    },
+    {
+        name: "Nắm Tay Nhau",
+        artist: "Lê Đức Anh",
+        img: "6",
+        src: "6"
+    },
+    {
+        name: "Sợ Ngày Mai Em Đi Mất",
+        artist: "Lê Đức Anh",
+        img: "7",
+        src: "7"
+    },
+    {
+        name: "Trót Trao Duyên",
+        artist: "Lê Đức Anh",
+        img: "8",
+        src: "8"
+    },
+    {
+        name: "Ngày Thơ ẤU",
+        artist: "Lê Đức Anh",
+        img: "9",
+        src: "9"
+    },
+      {
+        name: "Em Là Kẻ Đáng Thương",
+        artist: "Lê Đức Anh",
+        img: "10",
+        src: "10"
+    },
+                {      
+          name: "Vương Vấn",
+        artist: "Lê Đức Anh",
+        img: "11",
+        src: "11"
+    },
+                {
+             name: "Thuyền Quyên",
+        artist: "Lê Đức Anh",
+        img: "12",
+        src: "12"
+    },
+                {
+             name: "Cho Em Một Lần Yêu",
+        artist: "Lê Đức Anh",
+        img: "13",
+        src: "13"
+    },
+                {
+            name: "Như Anh Đã Thấy",
+        artist: "Lê Đức Anh",
+        img: "14",
+        src: "14"
+
+},
+          {
+          name: "Anh Mới Chính là Người Em yêu",
+        artist: "Lê Đức Anh",
+        img: "15",
+        src: "15"
+          },
+];
